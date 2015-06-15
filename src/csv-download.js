e60b59b362b7f3dc49d70784cede394a4eba2219
Christopher Luna <dev@christopher-luna.com>
2015-06-15 01:35:21 -0500

    Removed Polymer

--- src/csv-download.js
+++ src/csv-download.js
@@ -1,8 +1,29 @@
 /*jslint indent: 2, newcap: true */
-/*global document, window, Blob, Polymer*/
+/*global document, window, Blob, HTMLLinkElement*/
 (function () {
   'use strict';
-  var map = Function.prototype.call.bind(Array.prototype.map);
+  var map, csvDownloadPrototype;
+
+  csvDownloadPrototype = Object.create(HTMLLinkElement.prototype);
+  map = Function.prototype.call.bind(Array.prototype.map);
+
+  function setProperty(obj, name, def, fn) {
+    if (obj.getAttribute(name) === null) {
+      obj.setAttribute(name, obj.props[name] || def);
+    }
+    Object.defineProperty(obj, name, {
+      get : function () {
+        return obj.getAttribute(name) || obj.props[name] || def;
+      },
+      set : function (val) {
+        obj.props[name] = val;
+        obj.setAttribute(name, val);
+        if (typeof fn === 'function') {
+          fn.call(obj);
+        }
+      }
+    });
+  }
 
   function arrayToCsv(array, delimiter) {
     delimiter = delimiter || ',';
@@ -23,44 +44,33 @@
     }).join('\r\n');
   }
 
-  Polymer({
-    is : 'csv-download',
-    extends : 'a',
-    properties : {
-      delimiter : {
-        type : String,
-        observer : 'createURI',
-        value : ','
-      },
-      data : {
-        type : Array,
-        observer : 'createURI',
-        value : function () {
-          return [];
-        }
-      },
-      download : {
-        type : String,
-        reflectToAttribute : true,
-        value : 'data.csv'
-      }
-    },
-    createURI : function () {
-      this.csvString = arrayToCsv(this.data, this.delimiter);
-      Polymer.dom(this).setAttribute('href',
-          'data:application/octet-stream,' + encodeURIComponent(this.csvString));
-    },
-    ready : function () {
-      this.addEventListener('click', function (e) {
-        var data;
+  csvDownloadPrototype.createURI = function () {
+    this.csvString = arrayToCsv(this.data, this.delimiter);
+    this.setAttribute('href',
+        'data:text/csv,' + encodeURIComponent(this.csvString));
+  };
 
-        this.createURI();
-        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-          e.preventDefault();
-          data = new Blob([ this.csvString ]);
-          window.navigator.msSaveOrOpenBlob(data, this.download);
-        }
-      }, true);
-    }
+  csvDownloadPrototype.createdCallback = function () {
+    this.props = {};
+    this.data = this.data || [];
+    setProperty(this, 'download', 'data.csv', this.createURI);
+    setProperty(this, 'delimiter', ',', this.createURI);
+    this.createURI();
+
+    this.addEventListener('click', function (e) {
+      var data;
+
+      this.createURI();
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        e.preventDefault();
+        data = new Blob([ this.csvString ]);
+        window.navigator.msSaveOrOpenBlob(data, this.download);
+      }
+    }, true);
+  };
+
+  document.registerElement('csv-download', {
+    prototype : csvDownloadPrototype,
+    extends : 'a'
   });
 }());
